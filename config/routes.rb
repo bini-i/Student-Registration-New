b3b05a4304d9a9901e84155f8537172ee403669f
binyam hailmeskel <binyam.sh1@gmail.com>
2023-11-04 11:17:36 +0300

    Update user models     - remove registerable module so users can't self registerable
    Update routes
        - to include path for editing passwords since removing registerable
            module removed the default paths
    
    Update _navigation view to include path to Update password
    
    Update devise edit view to allow password editing based on the
    below wiki
    "https://github.com/heartcombo/devise/wiki/
    How-To:-Allow-users-to-edit-their-password#solution-2"

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
+
+  as :user do
+    get 'users/edit' => 'devise/registrations#edit', :as => 'edit_user_registration'    
+    patch 'users' => 'devise/registrations#update', :as => 'user_registration'            
+  end
+
   resources :students
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
