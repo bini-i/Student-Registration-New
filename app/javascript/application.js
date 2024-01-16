ce3f7b0d05b321a070ac33615fcc268f43bfbd5c
binyam hailmeskel <binyam.sh1@gmail.com>
2024-01-16 13:41:02 +0300

    Update application layout and index view - modify section button styling in index view - added tomselect to allow dropdown selection

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,5 +2,28 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+import TomSelect from "tom-select"
+
+import { Tab, initMDB } from "mdb-ui-kit";
+
+initMDB({ Tab });
 
 window.bootstrap = bootstrap
+window.TomSelect = TomSelect
+
+// document.addEventListener("DOMContentLoaded", function(){
+//     new TomSelect("#section_course_id",{
+//         create: false,
+//         sortField: {
+//             field: "text",
+//             direction: "asc"
+//         }
+//     });
+//     new TomSelect("#section_teacher_id",{
+//         create: false,
+//         sortField: {
+//             field: "text",
+//             direction: "asc"
+//         }
+//     });
+// });
